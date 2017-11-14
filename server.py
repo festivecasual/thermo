8060ed948ebb073d03186aa1a18b6712d410b2e1
Brandon McCalip <bmccalip@gmail.com>
2017-11-14 14:56:52 -0600

    Latest revisions

--- server.py
+++ server.py
@@ -4,6 +4,8 @@ import Adafruit_MAX31855.MAX31855 as MAX31855
 from flask import Flask
 app = Flask(__name__)
 
+import subprocess
+
 # Define a function to convert celsius to fahrenheit.
 def c_to_f(c):
         return c * 9.0 / 5.0 + 32.0
@@ -13,6 +15,16 @@ def snapshot():
 	sensor = MAX31855.MAX31855(spi=SPI.SpiDev(0, 0))
 	temp = sensor.readTempC()
 	return '{:d}'.format(int(c_to_f(temp)))
+
+@app.route('/shutdown')
+def shutdown():
+	subprocess.call(['sudo', 'poweroff'])
+	return 'ok'
+
+@app.after_request
+def origin_header(response):
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    return response
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=6124, debug=True)
